fbf1b1792582b6673c2ceb27ae59d822f830046d
Iury Oliveira <iury@rarolabs.com.br>
2021-04-25 15:17:43 -0300

    Food component refactor

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+import { StringMappingType } from "typescript";
+
+export interface FoodData {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+}
